fd304ddfbf0390d8f3a69e582ce1a692176d7ab3
Hyunsu Kim 김현수 <129834692+khs220507@users.noreply.github.com>
2025-07-29 23:01:19 +0900

    비전을 찾아서:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim() === '') return;
+    setTodos([...todos, {id : Date.now(), text, done: false}]);
+    setText('');
+  }
+  
+  const toggleDone = (id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    ));
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Todo App</h1>
+      <div className='todo-warpper'>
+      
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="할 일을 입력하세요"
+      />
+      <button onClick={addTodo}>추가</button>
+      </div>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id} className="todo-item">
+            <span
+              onClick={() => toggleDone(todo.id)}
+              className={`todo-text ${todo.done ? 'done' : ''}`}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)} className="add-button">
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
